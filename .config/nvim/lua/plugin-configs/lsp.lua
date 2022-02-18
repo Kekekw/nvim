8b95f3131c5ddf69dcaae5f2ceda4bb39af777d3
Quentin C <qchouleur@pobox.com>
2022-02-18 14:03:12 +0100

    Remove invalid field in nvim-lsp-ts-utils setup

--- .config/nvim/lua/plugin-configs/lsp.lua
+++ .config/nvim/lua/plugin-configs/lsp.lua
@@ -10,8 +10,8 @@ require "lspconfig".tsserver.setup {
     on_attach = function(client, bufnr)
         local ts_utils = require("nvim-lsp-ts-utils")
         ts_utils.setup {
-            debuger = true
         }
+
         ts_utils.setup_client(client)
     end
 }
